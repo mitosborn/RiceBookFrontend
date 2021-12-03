17720243e8592371595e45504fe7eb5a03f9c45c
Mitchell Osborn <mbo2@rice.edu>
2021-12-02 18:27:15 -0600

    Added frontend for editing post + commenting

--- ricebook/src/MainView/Post.js
+++ ricebook/src/MainView/Post.js
@@ -1,11 +1,22 @@
 import '../stylesheets/Post.css'
-import {Button, Col, Image, Row} from "react-bootstrap";
+import {Alert, Button, Col, Form, Image, Row} from "react-bootstrap";
 import Comment from "./Comment";
+import {useState} from "react";
 
+/*
 const comments = [{"name":"Jeff", "body":"This is so cool!"},{"name":"Gordon", "body":"I gotta go here sometime. Where is this?"},{"name":"Rudy", "body":"Interesting!"},{"name":"Anne", "body":"Very pretty photo! I need to visit here."}, {"name":"Bri", "body":"Placeholder commment."}]
+*/
 
-function Post({user, date, img, text}) {
+function Post({user, date, img, text, comments, pid}) {
     const defaultImg = "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg";
+    const [showAddComment, setShowAddComment] = useState(false);
+    const [newCommentText, setNewCommentText] = useState("");
+    const [showCommentAlert, setShowCommentAlert] = useState(false);
+
+    const [showUpdatePost, setShowUpdatePost] = useState(false);
+    const [updatedPostText, setUpdatedPostText] = useState("");
+    const [showUpdatedPostAlert, setUpdatedPostAlert] = useState(false);
+
     if(!date){
         date = new Date();
     }
@@ -42,27 +53,74 @@ function Post({user, date, img, text}) {
                             <div className={"commentBox"}>
                                 <h4 className={"commentTitle"}>Comments</h4>
                                 <div> 
-                                    {comments.map(c => <Comment key={c.name} user={c.name} body={c.body}/>)}
+                                    {comments.map(c => <Comment key={c.id} user={c.author} body={c.text} id={c.id} pid={pid}/>)}
                                 </div>
                             </div>
                     </Row>
                     <Row>
                         <Col className = {"col-12"}>
                             <div className="d-flex justify-content-center">
-                                <Button className="postButton btn btn-default mb-2">Comment</Button>
+                                <Button className="postButton btn btn-default mb-2"onClick={() => setShowUpdatePost(!showUpdatePost)}>{showUpdatePost ? "Close" : "Edit Post"}</Button>
                             </div>
                         </Col>
                     </Row>
                     <Row>
+                        {showUpdatePost &&<Form>
+                            {showUpdatedPostAlert && <Alert variant={'warning'} dismissible onClose={() => setUpdatedPostAlert(false)}>
+                                Comment is empty
+                            </Alert>}
+                            <Form.Group className="mb-3 whiteText" controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>Post</Form.Label>
+                                <Form.Control onChange={(e)=> setUpdatedPostText(e.target.value)} as="textarea" rows={1} />
+                            </Form.Group>
+                            <Row>
+                                <Col className = {"col-12"}>
+                                    <div className="d-flex justify-content-center">
+                                        <Button type="reset" className="addPostBtn btn btn-default mb-2">Clear Text</Button>
+                                        <Button onClick={() => console.log("Update post!!")}className="addPostBtn btn btn-default mb-2">Update Post</Button>
+                                    </div>
+                                </Col>
+                            </Row>
+
+                        </Form>}
+                    </Row>
+                    <Row>
                         <Col className = {"col-12"}>
                             <div className="d-flex justify-content-center">
-                                <Button className="postButton btn btn-default mb-2">Edit Post</Button>
+                                <Button className="postButton btn btn-default mb-2" onClick={() => setShowAddComment(!showAddComment)}>{showAddComment ? "Close" : "Comment"}</Button>
                             </div>
                         </Col>
                     </Row>
-                {/* Comment btns    */}
+                    <Row>
+                        {showAddComment &&<Form>
+                            {showCommentAlert && <Alert variant={'warning'} dismissible onClose={() => setShowCommentAlert(false)}>
+                                Comment is empty
+                            </Alert>}
+                            <Form.Group className="mb-3 whiteText" controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>Username</Form.Label>
+                                <Form.Control onChange={(e)=> setNewCommentText(e.target.value)} as="textarea" rows={1} />
+                            </Form.Group>
+                            <Row>
+                                <Col className = {"col-12"}>
+                                    <div className="d-flex justify-content-center">
+                                        <Button type="reset" className="addPostBtn btn btn-default mb-2">Clear Text</Button>
+                                        <Button onClick={() => console.log("Send comment!")}className="addPostBtn btn btn-default mb-2">Post</Button>
+                                    </div>
+                                </Col>
+                            </Row>
+
+                        </Form>}
+                    </Row>
+
+
+                    {/* Create comment box   */}
+                {/* Edit post box   */}
+
+                    {/* Comment btns    */}
                 </Col>
             </Row>
+
+
             {/*<Row>*/}
             {/*    <Col className={"col-7"}>*/}
             {/*        <div className={"postText"}>*/}
--- ricebook/src/MainView/PostContainer.js
+++ ricebook/src/MainView/PostContainer.js
@@ -4,8 +4,8 @@ function PostContainer({props}) {
     console.log(props);
     let posts;
     if(props && props.length != 0){
-         posts = props.map(({_id, pid, author, date, image, text}) => {
-            return <Post key={_id} user={author} date={date} img={image} text={text}/>
+         posts = props.map(({_id, pid, author, date, image, text, comments}) => {
+            return <Post key={_id} pid={pid} user={author} date={date} img={image} text={text} comments={comments}/>
         })
     }
 
